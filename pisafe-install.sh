32f8e5fced98b3c0292d0ef33d0261dbbdb4e749
Beau Woods <beauwoods@users.noreply.github.com>
2024-02-24 09:32:12 -0500

    Pretty substantive overhaul

--- pisafe-install.sh
+++ pisafe-install.sh
@@ -35,15 +35,16 @@ fi
 ## Functions
 ##########
 
-## find_window_manager() - Determine which window manager to use. Keep this above the function call
-## Source: https://unix.stackexchange.com/questions/64627/whiptail-or-dialog
+## find_window_manager() - Determine which window manager to use. 
 find_window_manager() {
-    read window <<< "$(which whiptail dialog 2> /dev/null)" # Check whether whiptail or dialog is installed, assign the first one found to '$window'
-    [[ "$window" ]] || {
-    echo 'Error: Neither whiptail nor dialog found' >&2 # If neither whiptail or dialog is installed, throw an error.
-    exit 1
-    }
-} # End find_window_manager()
+    window=$(which whiptail dialog 2> /dev/null | head -n 1) # Check whether whiptail or dialog is installed, assign the first one found to '$window'
+    if [[ -n "$window" ]]; then
+        echo "$window"
+    else
+        echo 'Error: Neither whiptail nor dialog found' >&2 If neither whiptail or dialog is installed, throw an error.
+        exit 1
+    fi
+}
 
 ## configure_ssh() - Gather configuration details for harening the ssh server
 configure_ssh() {
@@ -54,24 +55,36 @@ configure_ssh() {
 
     ## First, set 'ssh_key' to a non-null, invalid key value. Then while it's not valid and not null, iterate over this dialog until it's entered correctly. We first initialize the 'ssh_key' variable, then show the dialog until EITHER the variable is NULL (input is blank, indicating a desire to avoid doing anything) or we receive a valid SSH public key, as verified by ssh-keygen.
     ssh_key="variable initialization" # Set this to a STRING that is not a valid SSH key
-    valid_ssh_key="1" # Set this to 1, which is neither the status code for 'valid' nor 'invalid/failure' we set later
-    until [ -z "$ssh_key" ] || [ "$valid_ssh_key" -eq 0 ]
-    do
-        ssh_key=$($window --inputbox "SSH keys are access credentials based on strong cryptographic principles. You can learn more about them here: https://www.ssh.com/academy/ssh-keys \
-        \n \
-        \nEntering your SSH public key below will add the key to the 'authorized_keys' file. Leave the field blank to avoid changing this setting. \
-        \n" --title "Gathering SSH key" 25 80 3>&1 1>&2 2>&3)
-        ssh_key=$(echo "$ssh_key" | tr -d '\n') # Remove newline charachter ('\n') from the key, if it has any
-        #if [ -n "$ssh_key" ]; then break; fi # If the response is NULL, '$ssh_key' is NULL and the while loop breaks
-        echo "$ssh_key" | ssh-keygen -lf - 2>&1 1>&/dev/null # Test to see if the SSH key is valid
-        valid_ssh_key=$? # Store the exit status of the test as a variable 'valid_ssh_key' - 0 indicates a valid key, 255 indicates some kind of failure
-        if [ "$valid_ssh_key" -eq 255 ] && [ ! -z "$ssh_key" ]; then
-            $window --msgbox "You entered an invalid SSH public key. You will be prompted again. To skip this step, leave the field blank." --title "Invalid SSH key" 25 80 # If the SSH key is invalid
-        elif [ "$valid_ssh_key" -eq 0 ]; then
-            ssh_key_name=$($window --inputbox "Enter a name for the SSH key \
-            \n" --title "Gathering SSH key name" 25 80 3>&1 1>&2 2>&3) # Get a name for the SSH key to be written to the 'authrized_keys' file
-            $window --title "Disable SSH password authentication" --yesno "SSH key authentication is (typically) much stronger than password authentication, which is enabled by default. Would you like to disable password authentication?" 25 80 --no-button "No change" --yes-button "Disable password authentication" # Prompt to disable password authentication in the '/etc/ssh/sshd_config.d/' file that we write later
-            disable_ssh_password_authentication=$? # Set the variable to the desired choice. Yes=1; No=0
+    # Initialize a flag to track SSH key validity
+    valid_ssh_key=1
+
+    # Loop until a valid SSH key is entered or the user chooses to skip
+    while [ $valid_ssh_key -ne 0 ]; do
+        ssh_key=$($window --inputbox "Enter your SSH public key. SSH keys are strong access credentials. Learn more at: https://www.ssh.com/academy/ssh-keys\nLeave the field blank to skip." --title "SSH Public Key" 25 80 3>&1 1>&2 2>&3)
+        
+        # Check if the user chose to skip by entering an empty key
+        if [ -z "$ssh_key" ]; then
+            echo "No SSH key entered. Skipping SSH key setup."
+            break # Exit the loop and skip SSH key setup
+        fi
+
+        # Test the entered SSH key for validity
+        echo "$ssh_key" | ssh-keygen -lf - &>/dev/null
+        valid_ssh_key=$?
+        
+        if [ $valid_ssh_key -eq 0 ]; then
+            # The SSH key is valid; proceed with disabling password authentication
+            $window --title "Disable SSH Password Authentication" --yesno "Would you like to disable password authentication for SSH? This is recommended for increased security if you set up SSH keys." 10 60
+            
+            passwordAuthentication="yes"
+            if [ $? -eq 0 ]; then
+                # User chose to disable password authentication
+                passwordAuthentication="no"
+            fi
+        else
+            # The SSH key is invalid; inform the user and loop again
+            $window --msgbox "The SSH key you entered is invalid. Please try again." --title "Invalid SSH Key" 10 60
+            # Optionally reset variables if needed
         fi
     done
 
@@ -84,6 +97,45 @@ configure_ssh() {
     done
 
 } # End configure_ssh()
+
+configure_tailscale() {
+    # Tailscale exit node configuration
+    $window --title "Tailscale Exit Node" --yesno "Do you want this host to be a Tailscale exit node?" 10 60
+
+    if [ $? -eq 0 ]; then # Yes
+        printf "\nThis host will be able to route network traffic from other Tailscale nodes. Complete this choice in the Tailscale Machines configuration page. https://login.tailscale.com/admin/machines\n"
+        tailscale_exit_node="--advertise-exit-node"
+        echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
+        echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
+        sudo sysctl -p /etc/sysctl.conf
+    else # No
+        printf "\nThis host will NOT route network traffic from other Tailscale nodes.\n"
+        tailscale_exit_node=''
+    fi
+
+    # Tailscale SSH accessibility configuration
+    $window --title "Tailscale SSH Access" --yesno "Do you want this host to be accessible through Tailscale SSH?" 10 60
+
+    if [ $? -eq 0 ]; then # Yes
+        printf "\nThis host will be accessible through Tailscale SSH. For more information, see the documentation. https://tailscale.com/kb/1193/tailscale-ssh/\n"
+        tailscale_ssh="--ssh"
+    else # No
+        printf "\nThis host will NOT be accessible through Tailscale SSH.\n"
+        tailscale_ssh=''
+    fi
+
+    # Ask whether to start Tailscale
+    $window --title "Start Tailscale" --yesno "Do you want to start Tailscale now?" 8 50
+
+    if [ $? -eq 0 ]; then
+        # Commands to start Tailscale
+        echo "Starting Tailscale..."
+        tailscale up "$tailscale_exit_node" "$tailscale_ssh"
+        $window --msgbox "Tailscale has been started." 8 50
+    else
+        $window --msgbox "Tailscale has not been started. You can start it manually later." 8 50
+    fi
+}
 
 ## Set up the logfile and dump some information into it to get us started
 printf "==========Output from the \"%s\" script, run as %s on %s\n" "$script_name" "$last_command" "$(date)" > $output_log # Start the logfile, overwriting any file that existed before
@@ -123,8 +175,10 @@ printf "==========Exited PASSWORD CHANGE at %s\n" "$(date)" >> "$output_log"
 ####################
 ## 2. SSH Tasks
 ####################
-## 2a. Import SSH key - Test if the '.ssh' folder exists in the account root and, if not, create it, backup the existing 'authorized_keys' file, then place the key into the 'authorized_keys' file, with a comment on source and date.
+## Call the configure_ssh function to gather the necessary information for hardening the SSH server.
+configure_ssh
 
+## 2a. Import SSH key - Test if the '.ssh' folder exists in the account root and, if not, create it, backup the existing 'authorized_keys' file, then place the key into the 'authorized_keys' file, with a comment on source and date.
 printf "\n.....\nWorking: 2a. Import SSH key\n"
 if [ ! -d ~/.ssh ]; then # If the .ssh folder doesn't exist,
     mkdir ~/.ssh # Create it
@@ -149,15 +203,16 @@ then
         \nPermitRootLogin no \
         \nPubkeyAuthentication yes \
         \nAuthorizedKeysFile .ssh/authorized_keys \
-        \nPasswordAuthentication no \
+        \nPasswordAuthentication %s \
         \nKbdInteractiveAuthentication no\n\n" \
-        "$script_file_name" "$ssh_port"; } \
+        "$script_file_name" "$ssh_port" "$passwordAuthentication"; } \
     | sudo tee -a /etc/ssh/sshd_config.d/99-$script_name.conf > /dev/null # Add the config to the sshd_config.d folder
 fi
+
 printf "\n.....\nCompleted: 2b. Harden sshd config\n"
 ##
 ## 2c. Enable sshd - Load sshd automatically every time the server restarts
-sudo systemctl enable sshd
+sudo systemctl enable ssh
 
 ####################
 ## 3. Install Tailscale
@@ -166,65 +221,26 @@ sudo systemctl enable sshd
 printf "\n.....\nWorking: 3. Install Tailscale\n"
 curl -fsSL https://tailscale.com/install.sh | sh
 
+configure_tailscale
+
 #sudo tailscale up "$tailscale_exit_node" "$tailscale_ssh"
 printf ".....\nCompleted: 3. Install Tailscale\n"
 
 ####################
 ## 4. Software updates
 ####################
-## 4b. Install unattended-upgrades - OK, so the 'unattended-upgrades' package is great, in theory. But it's kinda broken out of the box, which is dumb. So we're going to install it and then write some config files so it does what it should have done from the start.
-printf "\n.....\nWorking: 4b. Install unattended-upgrades\n"
+## 4. Install unattended-upgrades - OK, so the 'unattended-upgrades' package is great, in theory. But it's kinda broken out of the box, which is dumb. So we're going to install it and then write some config files so it does what it should have done from the start.
+printf "\n.....\nWorking: 4. Install unattended-upgrades\n"
 sudo apt install unattended-upgrades
 if test -d /etc/apt/apt.conf.d/50unattended-upgrades
 then
      printf "\n# Line added by %s\nUnattended-Upgrade::Automatic-Reboot \"true\";\n\n" $script_file_name | sudo tee -a /etc/apt/apt.conf.d/50unattended-upgrades > /dev/null 
 fi
 sudo systemctl restart unattended-upgrades.service 
-printf "\n.....\nCompleted: 2b. Install unattended-upgrades\n"
+printf "\n.....\nCompleted: 4. Install unattended-upgrades\n"
 
-
-
-
-
-
-
-
-
-
-
-
-printf "\nAlright, we're done here! Did we have fun today? That's good.\n"
-printf "I've cleaned up what I can here, but you may have to do some on your own.\n" 
-printf " - Restart sshd (this will boot you from any current sshd sessions).\n"
-printf " - Reboot, if any of the updates said you'll need to.\n"
-printf " - Be sure to check the logfile at %s for more details.\n" "$output_log"
-printf "\n"
-printf "Okay, now to fire up Tailscale and send you on your way!\n"
-
-## Fire up Tailscale!
-while true; do
-    read -p "\n\nDo you want this host to be a Tailscale exit node [y/N]? " yn
-    case $yn in
-        [Yy]* ) printf "\nThis host will be able to route network traffic from other Tailscale nodes. Complete this choice in the Tailscale Machines configuration page. https://login.tailscale.com/admin/machines\n";
-            tailscale_exit_node="--advertise-exit-node"; # Enable the command line option
-            echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf; # Configure network traffic forwarding
-            echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf; # Configure network traffic forwarding
-            sudo sysctl -p /etc/sysctl.conf; # Load the updated 'sysctl.conf' file
-            break;;
-        * ) printf "\nThis host will NOT route network traffic from other Tailscale nodes.\n"; tailscale_exit_node='' break;; # Ensure the command line option is unset
-    esac
-done
-
-while true; do
-    read -p "\n\nDo you want this host to be accessible through Tailscale SSH [y/N]? " yn
-    case $yn in
-        [Yy]* ) printf "\nThis host will be accessible through Tailscale SSH. For more information, see the documentation. https://tailscale.com/kb/1193/tailscale-ssh/\n"; tailscale_ssh="--ssh"; break;; # Enable the command line option
-        * ) printf "\nThis host will NOT be accessible through Tailscale SSH.\n"; tailscale_ssh='' break;; # Ensure the command line option is unset
-    esac
-done
-
-
-
+# Final message before asking to start Tailscale
+$window --msgbox "Alright, we're done here! Did we have fun today? That's good.\n\nI've cleaned up what I can here, but you may have to do some on your own.\n - Restart sshd (this will boot you from any current sshd sessions).\n - Reboot, if any of the updates said you'll need to.\n - Be sure to check the logfile at $output_log for more details.\n\nOkay, now to fire up Tailscale and send you on your way!" 20 70
 
 ## Send exit message to the console
 script_exit_normal=true # Set the flag to indicate that the script exited normally
